d5b8c94e0f99e3ceca3a516f0654678691720161
Muhammad Refa Walada Mushtofa <aerefa55@gmail.com>
2026-01-20 15:31:20 +0700

    initial commit

--- src/public/index.php
+++ src/public/index.php
@@ -1,20 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+$app = require __DIR__.'/../bootstrap/app.php';
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+$kernel = $app->make(Kernel::class);
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+return function ($request) use ($kernel, $app) {
+    $laravelRequest = Request::createFromBase($request);
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    $response = $kernel->handle($laravelRequest);
 
-$app->handleRequest(Request::capture());
+    $kernel->terminate($laravelRequest, $response);
+
+    // 🔥 WAJIB untuk worker mode
+    $app->forgetInstance('request');
+    $app->forgetInstance('auth');
+    $app->forgetInstance('session');
+
+    return $response;
+};
