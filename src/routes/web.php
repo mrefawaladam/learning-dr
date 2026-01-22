b623badcb8233adb1317bcfa6c2ca24b9950e040
Muhammad Refa Walada Mushtofa <aerefa55@gmail.com>
2026-01-22 10:57:07 +0700

    implement franken php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+$bootTime = now();
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/cek-worker', function () use ($bootTime) {
+    return [
+        'boot_time' => $bootTime->toDateTimeString(), // Waktu server pertama kali nyala
+        'request_time' => now()->toDateTimeString(),  // Waktu saat kamu refresh halaman
+        'message' => 'Jika boot_time TIDAK BERUBAH saat direfresh, Worker Mode AKTIF!',
+    ];
 });
